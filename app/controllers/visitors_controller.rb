0106f8426b528181e554f3118ae9c63567cd1bd4
Max Gorin <gorinme@gmail.com>
2014-08-16 10:14:26 +0700

    Add Dashboard

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,12 @@
 class VisitorsController < ApplicationController
+  def index
+    @best_scores = build_best_scores
+    @scores = Score.order(created_at: :desc)
+  end
+
+  def build_best_scores
+    GameLevel.all.map do |l|
+      best_score = l.scores.order(value: :desc).first
+    end.compact!
+  end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,5 @@
 class Score < ActiveRecord::Base
   belongs_to :game_level
+  belongs_to :user
+  delegate :game, to: :game_level
 end
