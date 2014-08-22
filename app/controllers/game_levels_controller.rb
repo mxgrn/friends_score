1396d815b46f2ccbea0cab5419b0a0afa82f16a3
Max Gorin <gorinme@gmail.com>
2014-08-22 16:54:58 +0700

    More fine-tuned authorization

--- app/controllers/game_levels_controller.rb
+++ app/controllers/game_levels_controller.rb
@@ -1,5 +1,5 @@
 class GameLevelsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_game
   before_action :set_game_level, only: [:show, :edit, :update, :destroy]
 
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,5 @@
 class ScoresController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_user
   before_action :set_score, only: [:show, :edit, :update, :destroy]
 
